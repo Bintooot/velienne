225a1dfcf7a5fa6e77e56c130173ff00b0bc4bbb
RamDiv <142648607+Bintooot@users.noreply.github.com>
2025-06-02 01:01:30 +0800

    Adding Section: Sidebar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   const [active, isActive] = useState("home");
+  const [show, isShow] = useState(false);
 
   return (
     <nav className="navbar-wrapper">
@@ -13,7 +14,7 @@ const Navbar = () => {
         <span>Velienne</span>
       </div>
       <div className="horizontal-separator"></div>
-      <div className="mobile-menu-icon">
+      <div className="mobile-menu-icon" onClick={() => isShow(!show)}>
         <FaBars fontSize={25} />
       </div>
       <div className="nav-links">
@@ -46,6 +47,57 @@ const Navbar = () => {
             </a>
           </li>
           <li className="nav-item">
+            <a
+              href="#contact-us"
+              onClick={() => isActive("contact-us")}
+              className={active === "contact-us" ? "active" : ""}
+            >
+              Contact us
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className={`sidebar ${show ? "show" : "hide"}`}>
+        <div className="sidebar-header-container">
+          <div className="logo-wrapper">
+            <img src={logo} alt="logo.png" width={64} height={52} />
+            <span>Velienne</span>
+          </div>
+          <div className="horizontal-separator"></div>
+          <div className="mobile-menu-icon" onClick={() => isShow(!show)}>
+            <FaBars fontSize={25} />
+          </div>
+        </div>
+
+        <ul>
+          <li className="sidebar-nav-item">
+            <a
+              href="#home"
+              onClick={() => isActive("home")}
+              className={active === "home" ? "active" : ""}
+            >
+              Home
+            </a>
+          </li>
+          <li className="sidebar-nav-item">
+            <a
+              href="#about-us"
+              onClick={() => isActive("about-us")}
+              className={active === "about-us" ? "active" : ""}
+            >
+              About us
+            </a>
+          </li>
+          <li className="sidebar-nav-item">
+            <a
+              href="#menu"
+              onClick={() => isActive("menu")}
+              className={active === "menu" ? "active" : ""}
+            >
+              Menu
+            </a>
+          </li>
+          <li className="sidebar-nav-item">
             <a
               href="#contact-us"
               onClick={() => isActive("contact-us")}
